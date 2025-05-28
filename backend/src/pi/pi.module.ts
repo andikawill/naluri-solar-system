acfdc4150afa754bf00d22a7c245073e9c2f8179
willy <willyandika007@gmail.com>
2025-05-28 11:03:35 +0700

    feat: :sparkles: feat: setup backend with NestJS base

---
+++ backend/src/pi/pi.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PiService } from './pi.service';
+import { PiController } from './pi.controller';
+
+@Module({
+  providers: [PiService],
+  controllers: [PiController]
+})
+export class PiModule {}
